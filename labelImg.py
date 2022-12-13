fafb0def1845eb2a52e13c983849c1c8fd54a136
kevser <kevserozturk6129@gmail.com>
2022-12-14 02:15:57 +0300

    some libraries made comment line

--- labelImg.py
+++ labelImg.py
@@ -17,11 +17,11 @@ except ImportError:
     # Ref:
     # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
     # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
-    if sys.version_info.major >= 3:
+    if sys.version_info.major >= 3: #we check version for python
         import sip
         sip.setapi('QVariant', 2)
-    from PyQt4.QtGui import *
-    from PyQt4.QtCore import *
+"""     from PyQt4.QtGui import *
+    from PyQt4.QtCore import * """
 
 from libs.combobox import ComboBox
 from libs.default_label_combobox import DefaultLabelComboBox
@@ -32,14 +32,14 @@ from libs.settings import Settings
 from libs.shape import Shape, DEFAULT_LINE_COLOR, DEFAULT_FILL_COLOR
 from libs.stringBundle import StringBundle
 from libs.canvas import Canvas
-from libs.zoomWidget import ZoomWidget
+#from libs.zoomWidget import ZoomWidget 
 from libs.lightWidget import LightWidget
 from libs.labelDialog import LabelDialog
 from libs.colorDialog import ColorDialog
 from libs.labelFile import LabelFile, LabelFileError, LabelFileFormat
 from libs.toolBar import ToolBar
-from libs.pascal_voc_io import PascalVocReader
-from libs.pascal_voc_io import XML_EXT
+#from libs.pascal_voc_io import PascalVocReader 
+#from libs.pascal_voc_io import XML_EXT 
 from libs.yolo_io import YoloReader
 from libs.yolo_io import TXT_EXT
 from libs.create_ml_io import CreateMLReader
@@ -47,7 +47,7 @@ from libs.create_ml_io import JSON_EXT
 from libs.ustr import ustr
 from libs.hashableQListWidgetItem import HashableQListWidgetItem
 
-__appname__ = 'label'
+__appname__ = 'EaglEye LabelApp'
 
 
 class WindowMixin(object):
